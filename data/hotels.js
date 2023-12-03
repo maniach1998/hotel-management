62432cd412d5f40b9fe098b4b4ff8d16022fe344
Kshitij Gugale <kgugale@stevens.edu>
2023-12-03 13:23:31 -0500

    create function

--- data/hotels.js
+++ data/hotels.js
@@ -1,37 +1,45 @@
-import Hotel from '../schema/Hotel.js';
+import { checkId, checkString } from "../helpers.js";
+import Hotel from "../schema/Hotel.js";
+import User from "../schema/User.js";
 
 const create = async (name, manager) => {
-	// TODO: add validation for fields
-	// name: valid string
-	// manager: valid ObjectId
-	// manager: check if User with this ObjectId exists
+  // TODO: add validation for fields
+  // name: valid string
+  // manager: valid ObjectId
+  // manager: check if User with this ObjectId exists
+  // Validation
+  name = checkString(name, "Name");
+  manager = checkId(manager, "Manager");
+  const user = await User.findById(manager);
+  if (!user)
+    throw { status: 404, message: "User with this `Manager` doesn't exist!" };
 
-	const newHotel = new Hotel({ name, manager });
-	const hotel = await newHotel.save();
+  const newHotel = new Hotel({ name, manager });
+  const hotel = await newHotel.save();
 
-	if (!hotel) throw new Error("Couldn't create hotel!");
+  if (!hotel) throw new Error("Couldn't create hotel!");
 
-	return hotel;
+  return hotel;
 };
 
 const update = async (id, name, manager) => {
-	// TODO: add validation for fields
-	// id: valid ObjectId -> check if hotel exists
-	// name: valid string
-	// manager: valid ObjectId -> check if user exists
-	// return the updated Hotel
+  // TODO: add validation for fields
+  // id: valid ObjectId -> check if hotel exists
+  // name: valid string
+  // manager: valid ObjectId -> check if user exists
+  // return the updated Hotel
 };
 
 const remove = async (id) => {
-	// TODO: add validation for fields
-	// id: valid ObjectId -> check if hotel exists
-	// return the removed Hotel
+  // TODO: add validation for fields
+  // id: valid ObjectId -> check if hotel exists
+  // return the removed Hotel
 };
 
 const getAll = async () => {
-	const hotels = await Hotel.find().populate('manager');
+  const hotels = await Hotel.find().populate("manager");
 
-	return hotels;
+  return hotels;
 };
 
 export default { create, update, remove, getAll };
