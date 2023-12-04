414724f06b6de156248edb7a241d0ca243899aaf
Kshitij Gugale <kgugale@stevens.edu>
2023-12-04 17:59:44 -0500

    helpers.js hotels.js changes

--- data/hotels.js
+++ data/hotels.js
@@ -1,5 +1,7 @@
-import { checkId, checkString } from "../helpers.js";
+import { checkNumber } from "../helpers.js";
 import Hotel from "../schema/Hotel.js";
+import Room from "../schema/Room.js";
+
 import User from "../schema/User.js";
 
 const create = async (name, manager) => {
@@ -7,12 +9,7 @@ const create = async (name, manager) => {
   // name: valid string
   // manager: valid ObjectId
   // manager: check if User with this ObjectId exists
-  // Validation
-  name = checkString(name, "Name");
-  manager = checkId(manager, "Manager");
-  const user = await User.findById(manager);
-  if (!user)
-    throw { status: 404, message: "User with this `Manager` doesn't exist!" };
+  name = checkString(name, "name");
 
   const newHotel = new Hotel({ name, manager });
   const hotel = await newHotel.save();
@@ -22,18 +19,70 @@ const create = async (name, manager) => {
   return hotel;
 };
 
-const update = async (id, name, manager) => {
-  // TODO: add validation for fields
-  // id: valid ObjectId -> check if hotel exists
-  // name: valid string
-  // manager: valid ObjectId -> check if user exists
-  // return the updated Hotel
+const get = async (hotelId) => {
+  // TODO: find hotel with _id: hotelId
+  // hotelId: valid ObjectId -> check if hotel exists
+  // return hotel
+  // Validation
+  hotelId = checkId(hotelId, "hotel Id");
+
+  // Find and return the hotel
+  const hotel = await Hotel.findById(hotelId).populate("author");
+  if (!hotel) throw { status: 404, message: "Couldn't find this hotel!" };
+
+  return hotel;
 };
 
-const remove = async (id) => {
+const update = async (hotelId, name, description) => {
   // TODO: add validation for fields
-  // id: valid ObjectId -> check if hotel exists
+  // hotelId: valid ObjectId -> check if hotel exists
+  // name: valid string
+  // description: valid string
+  // return the updated Hotel
+
+  // Validation
+  hotelId = checkId(hotelId, "hotel Id");
+  name = checkString(name, "name");
+  description = checkString(description, "description");
+
+  // Update the hotel with new content
+  const updatedhotel = await Hotel.findByIdAndUpdate(
+    hotelId,
+    { name },
+    { description },
+    { returnDocument: "after" }
+  );
+  if (!updatedhotel)
+    throw {
+      status: 404,
+      message: "hotel with this hotelId doesn't exist!",
+    };
+
+  return updatedhotel.populate("author");
+};
+
+const remove = async (hotelId) => {
+  // TODO: add validation for fields
+  // hotelId: valid ObjectId -> check if hotel exists
   // return the removed Hotel
+
+  // Validation
+  hotelId = checkId(hotelId, "hotel Id");
+
+  // Find and delete the hotel
+  const deletedhotel = await Hotel.findByIdAndDelete(hotelId);
+  if (!deletedhotel)
+    throw {
+      status: 404,
+      message: "hotel with this hotelId doesn't exist!",
+    };
+
+  // Remove hotelId from the Hotel's hotels array
+  await Hotel.findByIdAndUpdate(deletedhotel.hotel, {
+    $pull: { hotels: deletedhotel._id },
+  });
+  // TODO: delete all replies of a hotel
+  return deletedhotel;
 };
 
 const getAll = async () => {
@@ -42,4 +91,133 @@ const getAll = async () => {
   return hotels;
 };
 
-export default { create, update, remove, getAll };
+const createRoom = async (hotelId, type, number, price) => {
+  // TODO: create a new Room in hotel with _id: hotelId
+  // hotelId: valid ObjectId -> check if hotel exists
+  // type: valid string
+  // number: valid number
+  // price: valid number
+
+  // Validation
+  hotelId = checkId(hotelId, "Hotel Id");
+  type = checkString(type, "Type");
+  number = checkString(number, "Number");
+  price = checkString(price, "Price");
+
+  const newRoom = new room({ type, number, price });
+  const room = await newRoom.save();
+
+  if (!room) throw new Error("Couldn't create room!");
+
+  return room;
+};
+
+const getRoom = async (hotelId, roomId) => {
+  // TODO: get room with _id: roomId of a hotel with _id: hotelId
+  // hotelId: valid ObjectId -> check if hotel exists
+  // roomId: valid ObjectId -> check if room exists
+  // return room
+  hotelId = checkId(hotelId, "Hotel Id");
+  roomId = checkId(roomId, "Room Id");
+
+  // Find and return the hotel
+  const room = await Hotel.findById(hotelId && roomId).populate("author");
+  if (!room) throw { status: 404, message: "Couldn't find this room!" };
+
+  return room;
+};
+
+const updateRoom = async (hotelId, roomId, type, number, price) => {
+  // TODO: update room with _id: roomId in hotel with _id: hotelId
+  // hotelId: valid ObjectId -> check if hotel exists
+  // roomId: valid ObjectId -> check if room exists
+  // Validation fields-
+  // type: valid string (optional)
+  // number: valid number (optional)
+  // price: valid number (optional)
+  // return updated room
+
+  // Validation
+  hotelId = checkId(hotelId, "hotel Id");
+  roomId = checkId(roomId, "Room Id");
+  type = checkString(type, "Type");
+  number = checkNumber(number, "Number");
+  price = checkNumber(price, "Price");
+  // Update the hotel with new content
+  const updatedroom = await Hotel.findByIdAndUpdate(
+    hotelId && roomId,
+    { type },
+    { number },
+    { price },
+    { returnDocument: "after" }
+  );
+  if (!updatedroom)
+    throw {
+      status: 404,
+      message: "room with this hotelId and roomId doesn't exist!",
+    };
+
+  return updatedroom.populate("author");
+};
+
+const removeRoom = async (hotelId, roomId) => {
+  // TODO: delete room with _id: roomId in hotel with _id: hotelId
+  // hotelId: valid ObjectId -> check if hotel exists
+  // roomId: valid ObjectId -> check if room exists
+  // return deleted room
+  hotelId = checkId(hotelId, "hotel Id");
+  roomId = checkId(roomId, "Room Id");
+
+  // Find and delete the hotel
+  const deletedroom = await Hotel.findByIdAndDelete(hotelId);
+  if (!deletedroom)
+    throw {
+      status: 404,
+      message: "room with this roomId doesn't exist!",
+    };
+
+  // Remove roomId from the room's rooms array
+  await Hotel.findByIdAndUpdate(deletedroom.hotel, {
+    $pull: { rooms: deletedroom._id },
+  });
+  // TODO: delete all replies of a hotel
+  return deletedroom;
+};
+
+const bookRoom = async (hotelId, roomId, bookedBy, bookedFrom, bookedTill) => {
+  // TODO: book room with _id: roomId in hotel with _id: hotelId
+  // hotelId: valid ObjectId -> check if hotel exists
+  // roomId: valid ObjectId -> check if room exists
+  // Validation fields-
+  // bookedBy: valid ObjectId -> check if user exists
+  // bookedFrom: valid Date -> check if date is in the future
+  // bookedTill: valid Date -> check if date is in the future AND after bookedFrom date
+  // return booking status and booked room
+
+  hotelId = checkId(hotelId, "hotel Id");
+  roomId = checkId(roomId, "Room Id");
+  bookedBy = checkId(bookedBy, "User Id");
+
+  bookedFrom = checkString(bookedFrom, "Check In");
+  bookedTill = checkString(bookedTill, "Check Out");
+};
+
+const getAllRooms = async (hotelId) => {
+  // TODO: get all rooms of a hotel with _id: hotelId
+  // hotelId: valid ObjectId -> check if hotel exists
+  // return rooms
+};
+
+export default {
+  create,
+  get,
+  update,
+  remove,
+  getAll,
+  createRoom,
+  getRoom,
+  updateRoom,
+  removeRoom,
+  bookRoom,
+  getAllRooms,
+};
--- helpers.js
+++ helpers.js
@@ -1,23 +1,42 @@
-import { ObjectId } from 'mongodb';
+import { ObjectId } from "mongodb";
 
 function checkString(value, name) {
-	if (!value || typeof value !== 'string')
-		throw new Error(`\`${name}\` must be a non-empty string!`);
+  if (!value || typeof value !== "string")
+    throw new Error(`\`${name}\` must be a non-empty string!`);
 
-	value = value.trim();
-	if (value.length === 0) throw new Error(`\`${name}\` cannot be empty string or just spaces!`);
+  value = value.trim();
+  if (value.length === 0)
+    throw new Error(`\`${name}\` cannot be empty string or just spaces!`);
 
-	return value;
+  return value;
 }
 
 function checkId(id, name) {
-	if (!id || typeof id !== 'string') throw new Error(`\`${name}\` must be a non-empty string!`);
+  if (!id || typeof id !== "string")
+    throw new Error(`\`${name}\` must be a non-empty string!`);
 
-	id = id.trim();
-	if (id.length === 0) throw new Error(`\`${name}\` cannot be empty string or just spaces!`);
-	if (!ObjectId.isValid(id)) throw new Error(`\`${name}\` is not a valid ObjectId!`);
+  id = id.trim();
+  if (id.length === 0)
+    throw new Error(`\`${name}\` cannot be empty string or just spaces!`);
+  if (!ObjectId.isValid(id))
+    throw new Error(`\`${name}\` is not a valid ObjectId!`);
 
-	return id;
+  return id;
 }
 
-export { checkString, checkId };
+function checkNumber(value, name) {
+  value = value.trim();
+  if (value.length === 0)
+    throw new Error(`\`${name}\` cannot be empty or just spaces!`);
+  value = Number(value);
+  if (!value || value == NaN)
+    throw new Error(`\`${name}\` must be a non-empty Number!`);
+
+  value = value.trim();
+  if (value.length === 0)
+    throw new Error(`\`${name}\` cannot be empty string or just spaces!`);
+
+  return value;
+}
+
+export { checkString, checkId, checkNumber };
