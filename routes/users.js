fbf162eeba3b408c5beeced1e5bafaa6e536a901
Manas Acharekar <manas.acharekar98@gmail.com>
2023-12-17 23:59:30 -0500

    minor bug fix

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,11 @@
-import { Router } from "express";
+import { Router } from 'express';
 const router = Router();
 
-import { userData } from "../data/index.js";
-import { checkCheckin } from "../helpers.js";
-import { checkAuthorized } from "../middleware.js";
+import { userData } from '../data/index.js';
+import { checkAuthorized } from '../middleware.js';
 //import { userDataFunction } from "../data/index.js";
 
-router.route("/").get(async (req, res) => {
+router.route('/').get(async (req, res) => {
 	// only for time-being testing purposes
 	// this method will be removed later
 	try {
@@ -22,10 +21,10 @@ router.route("/").get(async (req, res) => {
 // 	// TODO: get user profile using userId
 // });
 
-router.route("/hotels/userprofile").get(checkAuthorized, async (req, res) => {
+router.route('/hotels/userprofile').get(checkAuthorized, async (req, res) => {
 	try {
-		console.log("This is request", req);
-		console.log("This is response", res);
+		console.log('This is request', req);
+		console.log('This is response', res);
 		const userprofile = await userData.getProfile(User._id);
 		return userprofile;
 	} catch (e) {
@@ -34,7 +33,7 @@ router.route("/hotels/userprofile").get(checkAuthorized, async (req, res) => {
 });
 
 router
-	.route("/update")
+	.route('/update')
 	.get(async (req, res) => {
 		//Display update form
 	})
