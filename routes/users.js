6e967bb44f13096f5da45504ddcbd51298e7245e
dhruvin06 <dhruvin6102@gmail.com>
2023-12-17 22:54:23 -0500

    update

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
-import { Router } from 'express';
+import { Router } from "express";
 const router = Router();
 
-import { userData } from '../data/index.js';
+import { userData } from "../data/index.js";
+import { userDataFunction } from "../data/index.js";
 
-router.route('/').get(async (req, res) => {
+router.route("/").get(async (req, res) => {
 	// only for time-being testing purposes
 	// this method will be removed later
 	try {
@@ -15,17 +16,24 @@ router.route('/').get(async (req, res) => {
 	}
 });
 
-router.route('/:userId').get(async (req, res) => {
+router.route("/:userId").get(async (req, res) => {
 	// TODO: get user profile using userId
 });
 
+router.route("/userprofile").get(async (req, res) => {
+	try {
+		const userprofile = await userData.getProfile();
+		return userprofile;
+	} catch (e) {
+		return res.status(500).send({ error: e.message });
+	}
+});
+
 router
-	.route('/update')
+	.route("/update")
 	.get(async (req, res) => {
 		//Display update form
 	})
 	.patch(async (req, res) => {
 		//Post a form with the updated changes in the user details
 	});
-
-export default router;
